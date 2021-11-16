69d64ce90c17ff17c952d765f9159c0b4f1e4b5d
3119004730黄创境 <1026903031@qq.com>
2021-11-16 15:13:35 +0800

    add(drum): 添加架子鼓基本svg

--- src/pages/drums/index.jsx
+++ src/pages/drums/index.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { useNavigate } from 'react-router-dom';
-
+import Drum from './drum'
+import './index.less'
 
 function Drums() {
   const navigate = useNavigate(); // 跳转到别的页面 navigate('xxxpath')
 
   return (
-    <div>
-      drums
+    <div className='drum-box'>
+      <Drum
+
+
+      />
     </div>
   )
 }
