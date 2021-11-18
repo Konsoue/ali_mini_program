f6523812359955828b86085e03e1933fea0ccf6a
konsoue <874111752@qq.com>
2021-11-18 16:23:09 +0800

    feat(piano): 添加完成按钮功能，修复Storage和debounce

---
+++ src/components/Piano/map.js
@@ -0,0 +1,17 @@
+export const mapKeyCodeToNote = new Map([
+  [49, 'C'],
+  [50, 'D'],
+  [51, 'E'],
+  [52, 'F'],
+  [53, 'G'],
+  [54, 'A'],
+  [55, 'B'],
+  [81, "C#"],
+  [87, "D#"],
+  [69, "F#"],
+  [82, "G#"],
+  [84, "A#"],
+])
+
+// noteName 与 dom 之间的映射
+export const mapNoteToDom = new Map();
